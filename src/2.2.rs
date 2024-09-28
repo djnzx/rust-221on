51057099722d8eecfe09293143b05841e573e767
Мария <m.ryban@e-u.edu.ua>
2024-09-28 21:17:46 +0300

    3.4

--- src/2.2.rs
+++ src/2.2.rs
@@ -7,7 +7,7 @@ fn main() {
     println!("Success!");
 }
 //3
-//Fix the error below with least amount of modification
+//Fix the error below with last amount of modification
 fn main() {
     let x: i32 = 10;
     let y: i32 = 5;
@@ -15,4 +15,14 @@ fn main() {
         println!("The value of x is {} and value of y is {}", x, y);
     }
     println!("The value of x is {} and value of y is {}", x, y);
+}
+//4
+// Fix the error with the use of define_x
+fn main() {
+    define_x();
+}
+
+fn define_x() {
+    let x = "hello";
+    println!("{}, world", x);
 }
