601e04c98a9b92bb15944d173d648cb177db3c17
Мария <m.ryban@e-u.edu.ua>
2024-10-06 13:17:56 +0300

    Завдання 4.1

--- src/t401.rs
+++ src/t401.rs
@@ -3,9 +3,9 @@
 #[test]
 fn main1() {
     let x: i32 = 5;
-    let mut y;
+    let mut y = 5;
     y = x;
-    let z: i32 = 10; // Type of z ?
+    let z:i32 = 10; // Type of z ?
     println!("Success!");
 }
 //2
@@ -19,7 +19,7 @@ fn main2() {
 //3
 #[test]
 // Modify `assert_eq!` to make it work
-fn main3() {
+fn main() {
     let x: u32 = 5;
     assert_eq!("u32".to_string(), type_of(&x));
 
@@ -27,7 +27,7 @@ fn main3() {
 }
 // Get the type of given variable, return a string representation of the type  , e.g "i8", "u8", "i32", "u32"
 fn type_of<T>(_: &T) -> String {
-    format!("{}", std::any::type_name::<T>())
+    format!("{}", std::any::type_name::<T>())//i32
 }
 //4
 #[test]
@@ -39,20 +39,94 @@ fn main4() {
 }
 //5
 #[test]
+// Fix errors and panics to make it work
 fn main5() {
-    let v1 = 251_u8.wrapping_add(8); // обгортаємо результат при переповненні
-    let v2 = i8::checked_add(120, 8).unwrap_or(-1); // значення за замовчуванням у разі переповнення
-    println!("{},{}", v1, v2);
+    let v1:u16 = 251_u16 + 8;
+    let v2:i16 = i16::checked_add(251, 8).unwrap();
+    println!("{},{}",v1,v2);
 }
 //6
 #[test]
 // Modify `assert!` to make it work
 fn main6() {
-    let v = 1_024 + 0xff + 0o77 + 0b1111_1111;
+    let v = 1_024 + 0xff + 0o77 + 0b1111_1111;//1024 + 255 + 63 + 255
     assert!(v == 1597);
+
     println!("Success!");
 }
 //7
+#[test]
+// Fill the blank to make it work
+fn main7() {
+    let x: f64= 1_000.000_1; // ?
+    let y: f32 = 0.12; // f32
+    let z: f64 = 0.01_f64; // f64
 
+    assert_eq!(type_of(&x), "f64".to_string());
+    println!("Success!");
+}
+fn type_of1<T>(_: &T) -> String {
+    format!("{}", std::any::type_name::<T>())
+}
 
 //8
+#[test]
+fn main801() {
+    assert!(0.1_f32 + 0.2_f32==0.3_f32);
+    println!("Success!");
+}
+#[test]
+fn main802() {
+    assert!(0.1 as f32+0.2 as f32==0.3 as f32);
+    println!("Success!");
+}
+//9
+#[test]
+fn main9() {
+    let mut sum = 0;
+    for i in -3..2 {
+        sum += i
+    }
+    assert!(sum == -5);
+    for c in 'a'..='z' {
+        println!("{}",c as u8);
+    }
+}
+//10
+// Fill the blanks
+#[test]
+use std::ops::{Range, RangeInclusive};
+fn main10() {
+    assert_eq!((1..5), Range{ start: 1, end: 5 });
+    assert_eq!((1..=5), RangeInclusive::new(1, 5));
+
+    println!("Success!");
+}
+//11
+// Fill the blanks and fix the errors
+#[test]
+fn main11() {
+    // Integer addition
+    assert!(1u32 + 2u32 == 3u32);
+
+    // Integer subtraction
+    assert!(1i32 - 2i32 == -1i32);
+    assert!(1i8 - 2i8 == -1);
+
+    assert!(3 * 50 == 150);//i32
+
+    assert!(9.6 as f32 / 3.2 as f32 == 3.0 as f32); // error ! make it work
+
+    assert!(24 % 5 == 4);
+    // Short-circuiting boolean logic
+    assert!(true && false == false);
+    assert!(true || false == true);
+    assert!(!true == false);
+
+    // Bitwise operations
+    println!("0011 AND 0101 is {:04b}", 0b0011u32 & 0b0101);
+    println!("0011 OR 0101 is {:04b}", 0b0011u32 | 0b0101);
+    println!("0011 XOR 0101 is {:04b}", 0b0011u32 ^ 0b0101);
+    println!("1 << 5 is {}", 1u32 << 5);
+    println!("0x80 >> 2 is 0x{:x}", 0x80u32 >> 2);
+}
